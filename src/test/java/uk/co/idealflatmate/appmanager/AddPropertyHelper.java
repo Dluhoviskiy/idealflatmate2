d5166f04525f24f53104b5a8240eae90f3e49b5b
Dmitriy Lukhovitskiy <dmitriy197819781978@gmail.com>
2018-05-11 12:57:28 +0300

    added path to photo

--- src/test/java/uk/co/idealflatmate/appmanager/AddPropertyHelper.java
+++ src/test/java/uk/co/idealflatmate/appmanager/AddPropertyHelper.java
@@ -58,7 +58,7 @@ public class AddPropertyHelper {
 
     public void uploadPropertyPhoto() {
        //$("#uploadimageform-imagefiles").uploadFile(new File("C:\\Users\\Lenovo\\Documents\\Photos for testing\\Property1.jpg"));
-        $("#uploadimageform-imagefiles").uploadFile(new File("C:\\Users\\lukhovytskyi\\Documents\\GitHub\\idealflatmatetest\\src\\test\\resources\\Property1.jpg"));
+        $("#uploadimageform-imagefiles").uploadFile(new File("https://github.com/Aserdiuk2/idealflatmatetest/blob/master/src/test/resources/Property1.jpg"));
         $(".file-preview-frame.krajee-default.file-preview-initial.file-sortable.kv-preview-thumb ").shouldBe(Condition.visible);
     }
 

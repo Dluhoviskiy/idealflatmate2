7adb2e42042165f1875c92dd9e8396f0b769a35d
Dmitriy Lukhovitskiy <dmitriy197819781978@gmail.com>
2019-01-17 19:57:28 +0200

    footer_6

--- src/test/java/uk/co/idealflatmate/tests/FooterTest.java
+++ src/test/java/uk/co/idealflatmate/tests/FooterTest.java
@@ -148,7 +148,7 @@ public class FooterTest extends TestBase {
         footerHelper.verificationTwitter();
     }
 
-   @Test
+  // @Test
     public void footerInstagramLink() {
         pageUrlVerifLiveGoStage();
         helperBase.toHomePage();
